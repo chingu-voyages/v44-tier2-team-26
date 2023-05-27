2b903158cacf6280de63b348bac08e04fa1ca805
Lee Goullaud <egoullaud@gmail.com>
2023-05-27 13:09:07 -0700

    fixed className typo bug

--- components/UserControls.jsx
+++ components/UserControls.jsx
@@ -30,13 +30,13 @@ function UserControls() {
           required
         />
 
-        <label class="relative flex justify-between items-center group p-2 ">
+        <label className="relative flex justify-between items-center group p-2 ">
           Boolean 1?
           <input
             type="checkbox"
-            class="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md"
+            className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md"
           />
-          <span class="w-16 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-blue-950 rounded-full duration-300 ease-in-out peer-checked:bg-blue-400 after:w-8 after:h-8 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6 group-hover:after:translate-x-1"></span>
+          <span className="w-16 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-blue-950 rounded-full duration-300 ease-in-out peer-checked:bg-blue-400 after:w-8 after:h-8 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6 group-hover:after:translate-x-1"></span>
         </label>
 
         <label htmlFor="direction">Starting Direction</label>
