d2994f8b76e8fcbbf1a20135e6d457a1d0b4afd6
Lee Goullaud <egoullaud@gmail.com>
2023-05-27 14:03:49 -0700

    added useeffect to apply to ALL bots, console log

--- components/GameControlsLogic.jsx
+++ components/GameControlsLogic.jsx
@@ -1,44 +1,50 @@
 import React, { useEffect } from "react";
 
 const GameControlsLogic = ({operation, delay}) => {
-    const performLogic = (input1, input2) => {
-        switch (operation) {
-          case 'AND':
-            return input1 && input2;
-          case 'OR':
-            return input1 || input2;
-          case 'XOR':
-            return input1 !== input2;
-          case 'NOT':
-            return !input1;
-          default:
-            return false;
-        }
-      };
-      // Replace the following inputs with update function for bot object
-  const input1 = true;
-  const input2 = false;
+    useEffect(() => {
+        const performLogic = (input1, input2) => {
+          switch (operation) {
+            case 'AND':
+              return input1 && input2;
+            case 'OR':
+              return input1 || input2;
+            case 'XOR':
+              return input1 !== input2;
+            case 'NOT':
+              return !input1;
+            default:
+              return false;
+          }
+        };
+    //list of bots for testing
+        const bots = [
+          { input1: true, input2: false },
+          { input1: false, input2: true },
+          // Add more bots as needed
+        ];
+    
 
-  const result = performLogic(input1, input2);
+//create bot movement logic for ALL bots
+const botMovementTimeouts = bots.map((bot, index) => {
+    const { input1, input2 } = bot;
+    const result = performLogic(input1, input2);
 
- 
-
-//create bot movement logic
-useEffect(() => {
-    const botMovementTimeout = setTimeout(() => {
+    return setTimeout(() => {
       // Simulate bot movement
-      //replace with actual bot movement logic
-      console.log('Bot moved!');
+      console.log(`Bot ${index + 1} moved after ${delay} seconds with result: ${String(result)}`);
     }, delay * 1000);
+  });
 
     return () => {
-      clearTimeout(botMovementTimeout)
-    };
-  }, [delay]);
+        botMovementTimeouts.forEach((timeout) => {
+          clearTimeout(timeout);
+        });
+      };
+    }, [operation, delay]);
 
 
  //check console log output
- return console.log(`Result: ${String(result)}`)
+ return <div></div>
 
 }
 
