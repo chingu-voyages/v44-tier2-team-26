b728b95db93f750b7924368c070af61bdfb96257
Lee Goullaud <egoullaud@gmail.com>
2023-05-27 12:56:10 -0700

    added comments to performLogic function

--- components/GameControls.jsx
+++ components/GameControls.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // game controls
 // logic gate (AND/OR/blahblah)
@@ -6,13 +6,37 @@ import React from "react";
 // slider to select speed (pause-length)
 
 function GameControls() {
+  // define state for delay and logic operation
+  const [selectedOperation, setSelectedOperation] = useState('AND')
+  const [delay, setDelay] = useState(1)
+
+  // define boolean logic functions
+  // need to define bots in list or something
+  // finding that I'm actually not totally sure what the point of this is???
+  
+  const performLogic = (bot) => {
+    if (selectedOperation === 'AND') { 
+      return bot.positionX && bot.positionY
+    } else if (selectedOperation === "OR"){
+      return bot.positionX || bot.positionY
+    } else if (selectedOperation === 'XOR'){
+      return bot.positionX !== bot.positionY
+    } else if (selectedOperation === "NOT"){
+      return !bot.positionX
+    } else {return false}
+  }
+
+  //create function to set slider
+  const handleSliderChange = (e) => {
+    setDelay(Number(e.target.value))
+  }
   return (
     <div className="px-6 pb-10 mt-[3rem] bg-white text-black rounded-md">
       <div>
         <h1 className="py-2 text-center text-xl">Game Controls</h1>
         <form action="#" className="flex flex-col">
           {/* speed slider set default? */}
-          <label htmlFor="speed">Select Speed</label>
+          <label htmlFor="speed">Select Speed(in seconds)</label>
           <input
             className="accent-blue-950"
             required
@@ -20,13 +44,17 @@ function GameControls() {
             name="speed"
             id="speed"
             min={1}
-            max={10}
+            max={5}
             step={1}
+            value={delay}
+            onChange={handleSliderChange}
           />
           {/* logic gate */}
           <label htmlFor="logic-gate">Select Logic</label>
           <select
             required
+            value ={selectedOperation}
+            onChange={(e) => setSelectedOperation(e.target.value)}
             name="logic-gate"
             id="logic-gate"
             className="shadow-md rounded-md border-2 border-blue-950 focus:border-blue-500 p-1"
