76fbb28c9c853dccba5e9c3c588cf4928f95aa78
Lee Goullaud <egoullaud@gmail.com>
2023-05-27 15:51:22 -0700

    updated styling

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Link from "next/link";
 
 export default function Document() {
   return (
     <Html lang="en" className="bg-dark bg-fit">
-      <Head />
+      <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&family=Rubik:wght@400;600;700&display=swap" rel="stylesheet"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 
 import react from "react";
-import { Inter } from "next/font/google";
+
 import Link from "next/link";
-const inter = Inter({ subsets: ["latin"] });
+
 
 export default function Home() {
 
   return (
     <div className="flex items-center justify-center h-screen">
      <div className="flex flex-col items-center justify-center">
-    <h1 className="text-white text-5xl text-center">Welcome to BooleBot</h1>
+    <h1 className="font-header font-bold text-white text-5xl text-center">Welcome to BooleBot</h1>
    <Link href="/game">
    <button className="bg-[#46CDD2] text-white my-5 py-2 px-4 text-xl rounded-lg hover:bg-[#4De1e7] hover:ease-in-out hover:duration-500 ease-out duration-500">Let's Play</button>
    </Link> 
