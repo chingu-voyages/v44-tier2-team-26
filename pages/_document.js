586bbe6fc8fe784d66c04a3ccc03a7a8dd50f1c6
Lee Goullaud <egoullaud@gmail.com>
2023-05-27 14:46:01 -0700

    background image added

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className="bg-blue-950 text-white">
+    <Html lang="en" className="bg-dark bg-fit">
       <Head />
       <body>
         <Main />
