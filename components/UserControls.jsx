b6a827e4cc252f97d2ca145c3c201d5e872a183e
Lee Goullaud <egoullaud@gmail.com>
2023-05-27 15:51:45 -0700

    updated styling

--- components/UserControls.jsx
+++ components/UserControls.jsx
@@ -17,15 +17,15 @@ function UserControls() {
   }, [botName])
 
   return (
-    <div className="px-6 pb-10 mt-[3rem] bg-white text-black rounded-md">
+    <div className="px-6 pb-10 mt-[3rem] bg-[#08001A] text-white rounded-md">
       <form action="" className="flex flex-col">
         {/* User controls */}
         <h1 className="py-2 text-center text-xl">Bot Designer</h1>
         <label htmlFor="botName">Name Yer Bot</label>
         <input
-          className="shadow-md rounded-md border-2 border-blue-950 focus:border-blue-500 p-1"
+          className="shadow-md rounded-md border-2 border-[#00104B] focus:border-[#4De1e7] p-1"
           type="text"
-          placeholder="yer bot name"
+          placeholder=""
           onChange={(e)=> {setInputName(e.target.value)}}
           required
         />
@@ -36,7 +36,7 @@ function UserControls() {
             type="checkbox"
             className="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md"
           />
-          <span className="w-16 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-blue-950 rounded-full duration-300 ease-in-out peer-checked:bg-blue-400 after:w-8 after:h-8 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6 group-hover:after:translate-x-1"></span>
+          <span className="w-16 h-6 flex items-center flex-shrink-0 ml-4 p-1 bg-[#46CDD2] rounded-full duration-300 ease-in-out peer-checked:bg-[#4De1e7] after:w-8 after:h-8 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6 group-hover:after:translate-x-1"></span>
         </label>
 
         <label htmlFor="direction">Starting Direction</label>
@@ -44,14 +44,14 @@ function UserControls() {
           required
           name="direction"
           id="direction"
-          className="text-black border-2 border-blue-950 shadow-md rounded-md p-1"
+          className="text-black border-2 focus:border-[#4De1e7] shadow-md rounded-md p-1"
         >
           <option value="North">North</option>
           <option value="South">South</option>
           <option value="East">East</option>
           <option value="West">West</option>
         </select>
-        <button className="py-2 px-1 bg-blue-950 text-white rounded-full mt-4" onClick={(e) => {e.preventDefault(); setBotName(inputName)}} >
+        <button className="bg-[#46CDD2] text-white my-5 py-2 px-4 text-xl rounded-full hover:bg-[#4De1e7] hover:ease-in-out hover:duration-500 ease-out duration-500" onClick={(e) => {e.preventDefault(); setBotName(inputName)}} >
           Create Bot
         </button>
       </form>
